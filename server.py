14f3526d2dcb83feee34929ab24ea6ccd7c6518b
jadon-leung <146619444+jadon-leung@users.noreply.github.com>
2024-01-12 13:42:22 -0800

    little debugging

--- server.py
+++ server.py
@@ -4,9 +4,15 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
-#@app.route('/index')
-def index():
+
+
+@app.route('/index')
+def index(index):
     return render_template('index.html')
 
-if __name__ == "main":
+@app.route('/about')
+def about():
+    return render_template("index.html")
+
+if __name__ == "__main__":
     app.run(host = "0.0.0.0", port = 8000)
