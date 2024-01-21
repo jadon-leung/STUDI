ffb640a8451f365a1652742797e8947c59792f7b
jadon-leung <146619444+jadon-leung@users.noreply.github.com>
2024-01-21 01:59:28 -0800

    server update

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify, redirect, url_for
 
 
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -15,8 +16,17 @@ def about():
 def study():
     return render_template('study.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
+@app.route('/register')
+def login():
+    return render_template('regiter.html')
 
+@app.route('/home')
+def login():
+    return render_template('home.html')
 
 if __name__ == "__main__":
     #app.run(host = "0.0.0.0", port = 5500, debug = True)
