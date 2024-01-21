0a5912eaed94382f9824960b5872105edf6935db
jadon-leung <146619444+jadon-leung@users.noreply.github.com>
2024-01-20 23:35:09 -0800

    database installation
    set up Firebase and users can now start sesssions and it stores into firebase

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
-import pyrebase
+# import pyrebase
 
-config = {
-    apiKey: "AIzaSyAQ1j9ImVzvO7w_V3jc0KznSV52c4ssJ5g",
-  authDomain: "studi-31486.firebaseapp.com",
-  databaseURL: "https://studi-31486-default-rtdb.firebaseio.com",
-  projectId: "studi-31486",
-  storageBucket: "studi-31486.appspot.com",
-  messagingSenderId: "919376410608",
-  appId: "1:919376410608:web:40c513b7bdee38fcb24f35",
-  measurementId: "G-JGVEH7P7CP"
-}
+# config = {
+#     apiKey: "AIzaSyAQ1j9ImVzvO7w_V3jc0KznSV52c4ssJ5g",
+#   authDomain: "studi-31486.firebaseapp.com",
+#   databaseURL: "https://studi-31486-default-rtdb.firebaseio.com",
+#   projectId: "studi-31486",
+#   storageBucket: "studi-31486.appspot.com",
+#   messagingSenderId: "919376410608",
+#   appId: "1:919376410608:web:40c513b7bdee38fcb24f35",
+#   measurementId: "G-JGVEH7P7CP"
+# }
 
-app = pyrebase.initialize_app(config)
-database = app.database()
+# app = pyrebase.initialize_app(config)
+# database = app.database()
 
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 
 
 app = Flask(__name__)
@@ -15,19 +15,8 @@ def about():
 def study():
     return render_template('study.html')
 
-@app.route('/getdata', method = ['GET'])
-def get_data():
-    return "Data fetched successfully   "
 
-@app.route('/getjson')
-def get_json():
-    return jsonify({'key': 'value'})
 
-@app.route('/', methods=['POST'])
-def handle_data():
-    class_type = request.form['classes']
-    location = request.form['locations']
-    return render_template('/study.html#', c = class_type, location = location )
 
 if __name__ == "__main__":
     #app.run(host = "0.0.0.0", port = 5500, debug = True)
